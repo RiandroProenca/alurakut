21976500f933d07b9b0f64bb20479941fbd7885d
Riandro Proenca <73947142+RiandroProenca@users.noreply.github.com>
2021-07-13 19:58:49 -0300

    Modelo de rede social feita a partir de React e NextJs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/box'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea : 'profileArea'}} >
+      <Box> 
+        <img img src= "https://github.com/RiandroProenca.png" style={{ borderRadius: '8px' }} />
+        </Box>
+      </div>
+     <div className="welcomeArea" style={{ gridArea:' welcomeArea'}}>
+     <Box> 
+        Bem Vindo
+        </Box>
+     </div>
+     <div className="profileRelationsArea" style={{ gridArea : 'profileRelationsArea'}}>
+     <Box> 
+        Comunidade 
+        </Box>
+     </div>
+
+    </MainGrid>
+  )
 }
